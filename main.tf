2ff731fc6908ff60c19f4d8ce1d617e1cdd7abbe
sureshtarite <suresh.tarite@publicissapient.com>
2023-08-21 20:41:50 +0530

    tf changes07

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 terraform {
+  required_version = "~> 1.5.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = " 3.69" # for production-grade
     }
   }
 }
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  tenant_id       = var.tenant_id
+  tenant_id = var.tenant_id
   subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  client_id = var.client_id
+  client_secret = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
